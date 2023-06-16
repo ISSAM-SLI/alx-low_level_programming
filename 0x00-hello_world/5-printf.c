58ae1940c5e13717c6d32f90bfe747cc305f3fbb
ISSAM-SLI <issam.slimani.maroc@gmail.com>
2023-06-15 18:13:54 -0700

    Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Starting point of the program
+ * main - Entry point of the program
  *
- * This program prints a sentence that demonstrates proper grammar,
- 		resulting in a beautiful output.
- *
- * Return: Always 0 (indicating successful execution)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
