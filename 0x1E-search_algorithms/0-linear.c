39892734e34e9d98b9f787b3ec60780b1c0e3a51
ISSAM-SLI <issam.slimani.maroc@gmail.com>
2024-07-22 20:34:36 -0700

    Tasks

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -5,18 +5,18 @@
  * @size: size of array
  * @value: value to search on array
  * Return: index of found value or -1 on fail
-*/
+ */
 int linear_search(int *array, size_t size, int value)
 {
-        size_t i = 0;
+	size_t i = 0;
 
-        if (array == NULL)
-                return (-1);
-        for (i = 0; i < size; i++)
-        {
-                printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-                if (array[i] == value)
-                        return (i);
-        }
-        return (-1);
+	if (array == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }
